e178e0c65cc3e7d3b7ad80406dc8ea12c95beb78
hirohana <hirohanahaba@gmail.com>
2023-05-05 20:40:52 +0900

    image delete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -21,23 +21,6 @@ class OwnersController extends Controller
 
     public function index()
     {
-        // $data_now = Carbon::now();
-        // $data_parse = Carbon::parse(now());
-        // echo $data_now->year;
-        // echo $data_parse;
-
-        // $e_all = Owner::all();
-        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
-        // $q_first = DB::table('owners')->select('name')->first();
-
-        // $c_test = collect([
-        //     'name' => 'てすと'
-        // ]);
-
-        // var_dump($q_first);
-
-        // dd($e_all, $q_get, $q_first, $c_test);
-
         $owners = Owner::select('id', 'name', 'email', 'created_at')->paginate(3);
 
         return view('admin.owners.index', compact('owners'));
--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Image;
 use App\Http\Requests\UploadImageRequest;
 use App\Services\ImageService;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -79,6 +80,15 @@ class ImageController extends Controller
 
     public function destroy($id)
     {
-        //
+        $image = Image::findOrFail($id);
+        $filePath = 'public/products/' . $image->filename;
+
+        if (Storage::exists($filePath)) {
+            Storage::delete($filePath);
+        }
+
+        $image->delete();
+
+        return redirect()->route('owner.images.index')->with(['message' => '画像を削除しました。', 'status' => 'alert']);
     }
 }
