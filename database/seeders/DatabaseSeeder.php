744271f4124ca30b4911601a3cc788ccef8ec97f
hirohana <hirohanahaba@gmail.com>
2023-05-06 15:42:04 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call([
             AdminSeeder::class,
             OwnerSeeder::class,
             ShopSeeder::class,
-            ImageSeeder::class
+            ImageSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
